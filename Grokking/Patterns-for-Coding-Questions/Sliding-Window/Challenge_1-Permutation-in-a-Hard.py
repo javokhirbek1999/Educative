667f30adb655437be4fbcb12ae2abf0e0e01213b
Javokhirbek Khaydaraliev <54243963+javokhirbek1999@users.noreply.github.com>
2022-09-22 13:32:07 +0200

    Create Challenge_1-Permutation-in-a-Hard.py

---
+++ Grokking/Patterns-for-Coding-Questions/Sliding-Window/Challenge_1-Permutation-in-a-Hard.py
@@ -0,0 +1,43 @@
+"""
+n = len(s1)
+m = len(s2)
+
+Time: O(n)
+Space: O(m)
+"""
+
+from collections import Counter
+
+def find_permutation(str1, pattern):
+  
+  windowStart = 0
+
+  c = Counter(pattern)
+
+  vis = {}
+
+  for windowEnd in range(len(str1)):
+
+    right_char = str1[windowEnd]
+
+    if right_char not in vis:
+      vis[right_char] = 0
+    vis[right_char] += 1
+
+    if windowEnd - windowStart + 1 == len(pattern):
+      
+      if vis == c:
+        return True
+      
+      left_char = str1[windowStart]
+
+      vis[left_char] -= 1
+
+      if vis[left_char] == 0:
+        vis.pop(left_char)
+      
+      windowStart += 1
+  
+  return False
+
+  
